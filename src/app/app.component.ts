b38170341590b75739ac65ebd06f1999b4e1ad4f
Steven Mendez <stevenampaiz@gmail.com>
2023-03-08 11:54:29 -0600

    Directivas de Atributo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name: string = 'Test 2WDataBinding';
+  cities = ['Managua', 'Masaya', 'Leon', 'Granada', 'Chinandega', 'Esteli'];
+  name!: string;
   title = 'Dia 4 del reto';
   url =
     'https://th.bing.com/th/id/R.34f4c604cd9bfd44d39ece8f6a01363f?rik=7OIxdjJ6xOdnZA&pid=ImgRaw&r=0';
