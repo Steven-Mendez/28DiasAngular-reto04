ae47e27c2ec40c583366267d378ac957533836c4
Steven Mendez <stevenampaiz@gmail.com>
2023-03-08 10:21:16 -0600

    One way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'reto04';
+  title = 'Dia 4 del reto';
+  url =
+    'https://th.bing.com/th/id/R.34f4c604cd9bfd44d39ece8f6a01363f?rik=7OIxdjJ6xOdnZA&pid=ImgRaw&r=0';
 }
