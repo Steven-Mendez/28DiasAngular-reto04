6e2011f9fb8751dbb8f65af5c48a99ffb2d0ed6b
Steven Mendez <stevenampaiz@gmail.com>
2023-03-08 13:55:02 -0600

    Ciclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChange ->', changes);
+  }
+  ngOnInit(): void {
+    console.log('OnInit ->');
+  }
+  ngOnDestroy(): void {
+    console.log('Destroy ->');
+  }
   cities = ['Managua', 'Masaya', 'Leon', 'Granada', 'Chinandega', 'Esteli'];
   name!: string;
-  title = 'Dia 4 del reto';
+  title = 'Dia 8 del reto';
   url =
     'https://th.bing.com/th/id/R.34f4c604cd9bfd44d39ece8f6a01363f?rik=7OIxdjJ6xOdnZA&pid=ImgRaw&r=0';
 }
