480078bebf0906b2d1602b2738ce572daa51f4af
Steven Mendez <stevenampaiz@gmail.com>
2023-03-08 14:18:46 -0600

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,31 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnChanges, OnInit, OnDestroy {
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('OnChange ->', changes);
-  }
-  ngOnInit(): void {
-    console.log('OnInit ->');
-  }
-  ngOnDestroy(): void {
-    console.log('Destroy ->');
-  }
   cities = ['Managua', 'Masaya', 'Leon', 'Granada', 'Chinandega', 'Esteli'];
   name!: string;
   title = 'Dia 8 del reto';
   url =
     'https://th.bing.com/th/id/R.34f4c604cd9bfd44d39ece8f6a01363f?rik=7OIxdjJ6xOdnZA&pid=ImgRaw&r=0';
+  selection!: string;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChange ->', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit ->');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destroy ->');
+  }
+
+  onCityClicked(city: string): void {
+    console.log('Event ->', city);
+    this.selection = city;
+  }
+
+  onClear(): void {
+    this.selection = '';
+  }
 }
